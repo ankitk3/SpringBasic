176aaeb0b7ff541e78b80d95c15c9c58cf887084
Ankit Kalra <Ankit.Kalra@makemytrip.com>
2016-07-05 10:43:11 +0530

    Base code added, first running instance

--- src/main/java/com/my/config/SpringConfig.java
+++ src/main/java/com/my/config/SpringConfig.java
@@ -1,18 +1,47 @@
 package com.my.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
-import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
 @Configuration 
-@ComponentScan("com.concretepage") 
+@ComponentScan("com.my") 
 @EnableWebMvc
-public class SpringConfig {
+@EnableWebSecurity
+public class SpringConfig extends WebSecurityConfigurerAdapter{
 
+	@Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+	@Autowired
+    public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("bill").password("abc123").roles("USER");
+        auth.inMemoryAuthentication().withUser("admin").password("root123").roles("ADMIN");
+        auth.inMemoryAuthentication().withUser("dba").password("root123").roles("ADMIN","DBA");//dba have two roles.
+    }
+     
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+      http.authorizeRequests()
+        .antMatchers("/", "/login").permitAll() 
+        .antMatchers("/admin/**").access("hasRole('ADMIN')")
+        .antMatchers("/db/**").access("hasRole('ADMIN') and hasRole('DBA')")
+        .and().formLogin().loginPage("/login")
+        .and().exceptionHandling().accessDeniedPage("/Access_Denied");
+  
+    }
 }
--- src/main/java/com/my/controller/FirstController.java
+++ src/main/java/com/my/controller/FirstController.java
@@ -1,19 +1,66 @@
 package com.my.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class FirstController {
 	
-	@ResponseBody
-	@RequestMapping(value="/")
-	public String welcome(){
-		return "Hello, I am up and running.";
-	}
-	
+	@RequestMapping(value = { "/", "/home" }, method = RequestMethod.GET)
+    public String homePage(ModelMap model) {
+        model.addAttribute("greeting", "Hi, Welcome to mysite. ");
+        return "welcome";
+    }
+ 
+    @RequestMapping(value = "/admin", method = RequestMethod.GET)
+    public String adminPage(ModelMap model) {
+        model.addAttribute("user", getPrincipal());
+        return "admin";
+    }
+ 
+    @RequestMapping(value = "/db", method = RequestMethod.GET)
+    public String dbaPage(ModelMap model) {
+        model.addAttribute("user", getPrincipal());
+        return "dba";
+    }
+ 
+    @RequestMapping(value="/logout", method = RequestMethod.GET)
+       public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
+          Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+          if (auth != null){    
+             new SecurityContextLogoutHandler().logout(request, response, auth);
+          }
+          return "welcome";
+       }
+ 
+    @RequestMapping(value = "/Access_Denied", method = RequestMethod.GET)
+    public String accessDeniedPage(ModelMap model) {
+        model.addAttribute("user", getPrincipal());
+        return "accessDenied";
+    }
+     
+    private String getPrincipal(){
+        String userName = null;
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+ 
+        if (principal instanceof UserDetails) {
+            userName = ((UserDetails)principal).getUsername();
+        } else {
+            userName = principal.toString();
+        }
+        return userName;
+    }
     @ResponseBody
     @ExceptionHandler(Throwable.class)
     public String handleErrorPage(){
